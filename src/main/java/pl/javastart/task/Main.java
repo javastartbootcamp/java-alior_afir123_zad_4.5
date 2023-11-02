9a32eedec8baf5ccd1e334bc16451ae67b259e39
afir123 <kiri15@o2.pl>
2023-11-02 22:04:20 +0100

    zadanie1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,31 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Podaj liczbę");
+            number = scanner.nextInt();
+        } while (checkNumber(number));
+    }
 
+    private static boolean checkNumber(int number) {
+        if (number < 100) {
+            System.out.println("Podana liczba jest za mała");
+            return true;
+        } else if (number > 200) {
+            System.out.println("Podana liczba jest za duża");
+            return true;
+        } else if (number % 3 != 0) {
+            System.out.println("Liczba nie jest podzielna przez 3");
+            return true;
+        } else {
+            System.out.println("Twoja liczba jest ok");
+            return false;
+        }
     }
 }
